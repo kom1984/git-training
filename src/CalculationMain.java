a531797e5b2c3df5bc5370b5a59439cb815e9651
Komala <komalasaravanan84@gmail.com>
2022-07-24 19:49:59 +0200

    printing string and int

---
+++ src/CalculationMain.java
@@ -0,0 +1,13 @@
+
+public class CalculationMain {
+
+	public static void main(String[] args) {
+		String s="hello everyone";
+		System.out.println(s);
+		int a=35;
+		int b=30;
+		System.out.println(a+b);
+
+	}
+
+}
